aa0d3dd32e35d890a37aa8fe59a6d094b941bc57
samsonchen1989 <samsonchen1989@gmail.com>
2014-04-17 00:48:42 +0800

    Add camera control.
    1. Add camera control with only one FreeCamera right now.
    2. Add FreeCamera in RippleDeformer example.

--- examples/ripple_mesh_deformer/Game.cpp
+++ examples/ripple_mesh_deformer/Game.cpp
@@ -7,14 +7,19 @@
 
 #include "Game.h"
 #include "InputHandler.h"
+#include "opengl/FreeCamera.h"
 
 using namespace std;
 
 #define GL_CHECK_ERRORS assert(glGetError()==GL_NO_ERROR);
 
-//projection and modelview matrices
-glm::mat4  P = glm::mat4(1);
-glm::mat4 MV = glm::mat4(1);
+//shader reference
+GLSLShader shader;
+
+//vertex array and vertex buffer object IDs
+GLuint vaoID;
+GLuint vboVerticesID;
+GLuint vboIndicesID;
 
 const int NUM_X = 40; //total quads on X axis
 const int NUM_Z = 40; //total quads on Z axis
@@ -27,24 +32,138 @@ const float HALF_SIZE_Z = SIZE_Z/2.0f;
 //ripple displacement speed
 const float SPEED = 2;
 
+//for floating point imprecision
+const float EPSILON = 0.001f;
+const float EPSILON2 = EPSILON*EPSILON;
+
 //ripple mesh vertices and indices
 glm::vec3 vertices[(NUM_X+1)*(NUM_Z+1)];
 const int TOTAL_INDICES = NUM_X*NUM_Z*2*3;
 GLushort indices[TOTAL_INDICES];
 
-//camera transformation variables
-int state = 0, oldX=0, oldY=0;
-float rX=25, rY=-40, dist = -7;
+//projection and modelview matrices
+glm::mat4  P = glm::mat4(1);
+glm::mat4 MV = glm::mat4(1);
+
+//camera tranformation variables
+int state = 1, oldX=0, oldY=0;
+float rX=0, rY=0, fov = 45;
 
 //current time
 float time = 0;
 
-//vertex array and vertex buffer object IDs
-GLuint vaoID;
-GLuint vboVerticesID;
-GLuint vboIndicesID;
+//delta time
+float dt = 0;
+
+//timing related variables
+float last_time=0, current_time =0;
+
+//free camera instance
+CFreeCamera cam;
+
+int count = 0;
+
+//mouse filtering support variables
+const float MOUSE_FILTER_WEIGHT=0.75f;
+const int MOUSE_HISTORY_BUFFER_SIZE = 10;
+
+//mouse history buffer
+glm::vec2 mouseHistory[MOUSE_HISTORY_BUFFER_SIZE];
+
+float mouseX=0, mouseY=0; //filtered mouse values
+
+//flag to enable filtering
+bool useFiltering = true;
 
 Game* Game::s_pInstance = 0;
+
+//mouse move filtering function
+void filterMouseMoves(float dx, float dy) {
+    for (int i = MOUSE_HISTORY_BUFFER_SIZE - 1; i > 0; --i) {
+        mouseHistory[i] = mouseHistory[i - 1];
+    }
+
+    // Store current mouse entry at front of array.
+    mouseHistory[0] = glm::vec2(dx, dy);
+
+    float averageX = 0.0f;
+    float averageY = 0.0f;
+    float averageTotal = 0.0f;
+    float currentWeight = 1.0f;
+
+    // Filter the mouse.
+    for (int i = 0; i < MOUSE_HISTORY_BUFFER_SIZE; ++i)
+    {
+        glm::vec2 tmp=mouseHistory[i];
+        averageX += tmp.x * currentWeight;
+        averageY += tmp.y * currentWeight;
+        averageTotal += 1.0f * currentWeight;
+        currentWeight *= MOUSE_FILTER_WEIGHT;
+    }
+
+    mouseX = averageX / averageTotal;
+    mouseY = averageY / averageTotal;
+}
+
+void handleInput()
+{
+    //handle keyboard input
+    if (TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_W)) {
+        cam.Walk(dt);
+    }
+
+    if (TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_S)) {
+        cam.Walk(-dt);
+    }
+
+    if (TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_A)) {
+        cam.Strafe(-dt);
+    }
+
+    if (TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_D)) {
+        cam.Strafe(dt);
+    }
+
+    if (TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_Q)) {
+        cam.Lift(dt);
+    }
+
+    if (TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_V)) {
+        cam.Lift(-dt);
+    }
+
+    glm::vec3 t = cam.GetTranslation(); 
+    if(glm::dot(t,t)>EPSILON2) {
+        cam.SetTranslation(t*0.95f);
+    }
+
+    //handle mouse position change
+    Vector2D *mouseVector = TheInputHandler::Instance()->getMousePosition();
+    int x = mouseVector->getX();
+    int y = mouseVector->getY();
+
+    if (x == 0 && y == 0) {
+        return;
+    }
+    
+    if (state == 0) {
+        fov += (y - oldY)/5.0f;
+        cam.SetupProjection(fov, cam.GetAspectRatio());
+    } else {
+        rY += (y - oldY)/10.0f;
+        rX += (oldX - x)/10.0f;
+        if(useFiltering)
+            filterMouseMoves(rX, rY);
+        else { 
+            mouseX = rX;
+            mouseY = rY;
+        }
+        cam.Rotate(mouseX, mouseY, 0);
+    }
+
+    oldX = x;
+    oldY = y;
+}
 
 Game::Game():
 m_pWindow(0),
@@ -102,6 +221,10 @@ GAME_STATUS_TAG Game::init(const char* title, int xpos, int ypos, int width, int
         return GAME_ERROR_SDL_INIT_FAIL; //SDL could not initialize
     }
 
+    SDL_WarpMouseInWindow(m_pWindow, width / 2, height / 2);
+    oldX = width / 2;
+    oldY = height / 2;
+
     m_bRunning = true;
 
     m_pGameStateMachine = new GameStateMachine();
@@ -111,31 +234,31 @@ GAME_STATUS_TAG Game::init(const char* title, int xpos, int ypos, int width, int
     glEnable(GL_DEPTH_TEST);
     // Accept fragment if it closer to the camera than the former one
     glDepthFunc(GL_LESS);
-    //set the polygon mode to render lines, no fill
+
+    //set the polygon mode to render lines
     glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     //GL_CHECK_ERRORS
     //load shader
-    m_pShader->LoadFromFile(GL_VERTEX_SHADER, "shaders/shader.vert");
-    m_pShader->LoadFromFile(GL_FRAGMENT_SHADER, "shaders/shader.frag");
+    shader.LoadFromFile(GL_VERTEX_SHADER, "shaders/shader.vert");
+    shader.LoadFromFile(GL_FRAGMENT_SHADER, "shaders/shader.frag");
     //compile and link shader
-    m_pShader->CreateAndLinkProgram();
-    m_pShader->Use();
-        //add shader attribute and uniforms
-        m_pShader->AddAttribute("vVertex");
-        m_pShader->AddUniform("MVP");
-        m_pShader->AddUniform("time");
-    m_pShader->UnUse();
+    shader.CreateAndLinkProgram();
+    shader.Use();
+    //add shader attribute and uniforms
+    shader.AddAttribute("vVertex");
+    shader.AddUniform("MVP");
+    shader.AddUniform("time");
+    shader.UnUse();
+
     //GL_CHECK_ERRORS
 
-    P = glm::perspective(45.0f, (GLfloat)width/height, 1.f, 1000.f);
-
     //setup plane geometry
+    //setup plane vertices
     int count = 0;
     int i=0, j=0;
     for( j=0;j<=NUM_Z;j++) {
         for( i=0;i<=NUM_X;i++) {
-            // i/NUM_X, [0, 1]; (i/NUM_X * 2 - 1), [-1, 1]; final range:[-HALF_SIZE_X, HALF_SIZE_X]
             vertices[count++] = glm::vec3( ((float(i)/(NUM_X-1)) *2-1)* HALF_SIZE_X, 0, ((float(j)/(NUM_Z-1))*2-1)*HALF_SIZE_Z);
         }
     }
@@ -160,53 +283,75 @@ GAME_STATUS_TAG Game::init(const char* title, int xpos, int ypos, int width, int
 
     //GL_CHECK_ERRORS
 
-    //setup triangle vao and vbo stuff
+    //setup plane vao and vbo stuff
     glGenVertexArrays(1, &vaoID);
     glGenBuffers(1, &vboVerticesID);
     glGenBuffers(1, &vboIndicesID);
 
     glBindVertexArray(vaoID);
-        glBindBuffer(GL_ARRAY_BUFFER, vboVerticesID);
-        //pass triangle verteices to buffer object
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices[0], GL_STATIC_DRAW);
-        cerr << "size:" << sizeof(vertices) << endl;
-        //GL_CHECK_ERRORS
-        //enable vertex attribute array for position
-        glEnableVertexAttribArray((*m_pShader)["vVertex"]);
-        cerr << "va pointer:" << (*m_pShader)["vVertex"] << endl;
-        glVertexAttribPointer((*m_pShader)["vVertex"], 3, GL_FLOAT, GL_FALSE,0,0);
 
-        //pass indices to element array buffer
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vboIndicesID);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), &indices[0], GL_STATIC_DRAW);
-        //GL_CHECK_ERRORS
+    glBindBuffer (GL_ARRAY_BUFFER, vboVerticesID);
+    //pass plane vertices to array buffer object
+    glBufferData (GL_ARRAY_BUFFER, sizeof(vertices), &vertices[0], GL_STATIC_DRAW);
+    //GL_CHECK_ERRORS
+    //enable vertex attrib array for position
+    glEnableVertexAttribArray(shader["vVertex"]);
+    glVertexAttribPointer(shader["vVertex"], 3, GL_FLOAT, GL_FALSE,0,0);
+    //GL_CHECK_ERRORS
+    //pass the plane indices to element array buffer
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vboIndicesID);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), &indices[0], GL_STATIC_DRAW);
+    //GL_CHECK_ERRORS
+
+    //setup camera
+    //setup the camera position and look direction
+    glm::vec3 p = glm::vec3(5);
+    cam.SetPosition(p);
+    glm::vec3 look =  glm::normalize(p);
+
+    //rotate the camera for proper orientation
+    float yaw = glm::degrees(float(atan2(look.z, look.x)+M_PI));
+    float pitch = glm::degrees(asin(look.y));
+    rX = yaw;
+    rY = pitch;
+    if(useFiltering) {
+        for (int i = 0; i < MOUSE_HISTORY_BUFFER_SIZE ; ++i) {
+            mouseHistory[i] = glm::vec2(rX, rY);
+        }
+    }
+
+    cam.Rotate(rX,rY,0);
+    cam.SetupProjection(45, (GLfloat)width/height);
+
+    cout<<"Initialization successfull"<<endl;
 
     return GAME_INIT_SUCCESS;
 }
 
 void Game::render()
 {
-    time = SDL_GetTicks() / 1000.0f * SPEED;
+    last_time = current_time;
+    current_time = SDL_GetTicks() / 1000.0f;
+    dt = current_time - last_time;
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    //clear color buffer and depth buffer
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 
-    //set teh camera viewing transformation
-    //MV = (T * (Rx * Ry))
-    glm::mat4 T		= glm::translate(glm::mat4(1.0f),glm::vec3(0.0f, 0.0f, dist));
-    glm::mat4 Rx	= glm::rotate(T,  rX, glm::vec3(1.0f, 0.0f, 0.0f));
-    glm::mat4 MV	= glm::rotate(Rx, rY, glm::vec3(0.0f, 1.0f, 0.0f));
+    //set the camera transformation
+    glm::mat4 MV	= cam.GetViewMatrix();
+    glm::mat4 P     = cam.GetProjectionMatrix();
     glm::mat4 MVP	= P*MV;
 
-    //bind the shader
-    m_pShader->Use();
-        //pass the shader uniform
-        glUniformMatrix4fv((*m_pShader)("MVP"), 1, GL_FALSE, glm::value_ptr(P*MV));
-        //cerr << "time:" << time << endl;
-        glUniform1f((*m_pShader)("time"), time);
-        //drwa triangle
-        glDrawElements(GL_TRIANGLES, TOTAL_INDICES, GL_UNSIGNED_SHORT, 0);
+    //bind the shader 
+    shader.Use();
+    //set the shader uniforms
+    glUniformMatrix4fv(shader("MVP"), 1, GL_FALSE, glm::value_ptr(MVP));
+    glUniform1f(shader("time"), current_time * 2);
+    //draw the mesh triangles
+    glDrawElements(GL_TRIANGLES, TOTAL_INDICES, GL_UNSIGNED_SHORT, 0);
+
     //unbind the shader
-    m_pShader->UnUse();
+    shader.UnUse();
 
     SDL_GL_SwapWindow(m_pWindow);
 }
@@ -221,9 +366,6 @@ void Game::clean()
     m_pGameStateMachine = 0;
 
     m_pShader->DeleteShaderProgram();
-    glDeleteBuffers(1, &vboVerticesID);
-    glDeleteBuffers(1, &vboIndicesID);
-    glDeleteVertexArrays(1, &vaoID);
 
     delete m_pGameStateMachine;
     delete m_pShader;
@@ -241,6 +383,7 @@ void Game::quit()
 void Game::handleEvents()
 {
     TheInputHandler::Instance()->update();
+    handleInput();
 }
 
 void Game::update()
